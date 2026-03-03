a578e2c330d4070d9e5888a1b016f4513ce43899
Rav <ravanabba04@gmail.com>
2026-03-03 22:00:52 +0100

    oblig 3

---
+++ oblig/oblig3.cpp
@@ -0,0 +1,64 @@
+/**
+ *  obligatorisk oppgave 3
+ * 
+ * 
+ * 
+ * @file oblig3.cpp
+ * @author Ravan Abbasov
+ */
+
+ #include <iostream>    //cin, cout
+ #include <string>      
+ #include <vector>
+ #include <fstream>     // lese og skrive fra fil
+ #include <list>
+ #include "LesData2.h" 
+
+using namespace std;
+
+enum isType {Sorbe, Granite, Slush};
+
+class Iskrem {
+    private:
+    string smak;
+    int pris;
+
+    public:
+    // constructor
+    Iskrem(ifstream& inn) { inn >> smak >> pris; }
+
+    virtual void lesFraFil(ifstream& inn) { inn >> smak >> pris; }
+
+    virtual void skrivTilSkjerm() {cout << "smak: " << smak << "pris: " << pris;}
+    virtual void skrivTilFil(ofstream& ut) {ut << smak << ' ' << pris << "\n";}
+};
+
+class Sorbet : public Iskrem {
+    private:
+    isType sorbet;
+
+    public:
+    Sorbet(ifstream& inn);
+    virtual void lesFraFil(ifstream& inn);
+    virtual void skrivTilSkjerm();
+    virtual void skrivTilFIl(ofstream& ut);
+    
+};
+
+class Floteis : public Iskrem {
+    private:
+    bool vegan;
+
+    public: 
+    Floteis(ifstream& inn);
+    virtual void lesFraFil(ifstream& inn);
+    virtual void skrivTilSkjerm();
+    virtual void skrivTilFil(ofstream& ut);
+
+};
+
+class Isbil {
+    private:
+    string sted;
+
+};
