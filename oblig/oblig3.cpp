ff6e94b68072d7fc7daade633a7f14c3e6c3590b
Rav <ravanabba04@gmail.com>
2026-03-08 01:48:59 +0100

    ferdig med implimentasjon av fleste klasser. bare isbil igjen. holder på med constructor nå.

--- oblig/oblig3.cpp
+++ oblig/oblig3.cpp
@@ -18,20 +18,25 @@ using namespace std;
 
 enum isType {Sorbe, Granite, Slush};
 
+
+// ============================================================
+//  Klassedeklarasjoner
+// ============================================================
+
 class Iskrem {
     private:
     string smak;
     int pris;
 
     public:
-    // constructor
-    Iskrem(ifstream& inn) { inn >> smak >> pris; }
+    Iskrem(ifstream& inn);
+    virtual void lesFraFil(ifstream& inn);
 
-    virtual void lesFraFil(ifstream& inn) { inn >> smak >> pris; }
-
-    virtual void skrivTilSkjerm() {cout << "smak: " << smak << "pris: " << pris;}
-    virtual void skrivTilFil(ofstream& ut) {ut << smak << ' ' << pris << "\n";}
+    virtual void skrivTilSkjerm() const;
+    virtual void skrivTilFil(ofstream& ut);
 };
+
+// ------------------------------------------------------------
 
 class Sorbet : public Iskrem {
     private:
@@ -45,6 +50,8 @@ class Sorbet : public Iskrem {
     
 };
 
+// ------------------------------------------------------------
+
 class Floteis : public Iskrem {
     private:
     bool vegan;
@@ -56,6 +63,8 @@ class Floteis : public Iskrem {
     virtual void skrivTilFil(ofstream& ut);
 
 };
+
+// ------------------------------------------------------------
 
 class Isbil {
     private:
@@ -79,8 +88,126 @@ int main() {
 
 }
 
+// ============================================================
+//  Iskrem – implementasjon
+// ============================================================
+
+
+/**
+ * constructor som leser smak og pris fra fil.
+ */
+Iskrem::Iskrem(ifstream& inn) { 
+    inn >> smak >> pris; 
+}
+
+/**
+ * Leser alle smak og pris fra fil.
+ */
+void Iskrem::lesFraFil(ifstream& inn) {
+     inn >> smak >> pris; 
+}
+
+/**
+ * skriver til skjerm smak og pris.
+ */
+void Iskrem::skrivTilSkjerm() const {
+    cout << "smak: " << smak << "pris: " << pris;
+}
+
+/**
+ * skriver smak og pris til fil.
+ */
+void Iskrem::skrivTilFil(ofstream& ut) {
+    ut << smak << ' ' << pris << ' ' << "\n";
+}
+
+
+// ============================================================
+//  Sorbet – implementasjon
+// ============================================================
+
+
+/**
+ * constructor, leser smak, pris og sorbetype fra fil
+ */
 Sorbet::Sorbet(ifstream & inn) : Iskrem(inn) {
     int type;
     inn >> type;
     sorbet = static_cast<isType>(type);
+}
+
+/**
+ * leser alle sorbet data fra fil.
+ */
+void Sorbet::lesFraFil(ifstream & inn) {
+    int input;
+
+    Iskrem::lesFraFil(inn);     //leser smak og pris
+  
+    inn >> input;                //les sorbet type
+    sorbet = static_cast<isType>(input);    //konverter til enum
+}
+
+/**
+ * skriver ut alle sorbet data til skjerm
+ */
+void Sorbet::skrivTilSkjerm() {
+    Iskrem::skrivTilSkjerm();
+    cout << "type: ";
+    switch (sorbet) {
+        case Sorbe: cout << "Sorbe"; break;
+        case Granite: cout << "Granite"; break;
+        case Slush: cout << "Slush"; break;
+    }
+}
+
+/**
+ * skriver ut alle Sorbet data til fil
+ */
+void Sorbet::skrivTilFil(ofstream& ut) {
+    Iskrem::skrivTilFil(ut);
+    ut << sorbet;
+}
+
+// ============================================================
+//  Floteis – implementasjon
+// ============================================================
+
+/**
+ * constructor, leser smak, pris og vengastatus fra fil.
+ */
+Floteis::Floteis(ifstream& inn) : Iskrem(inn) {
+    inn >> vegan;
+}
+
+/**
+ * leser alle fløteis data fra fil.
+ */
+void Floteis::lesFraFil(ifstream& inn) {
+    Iskrem::lesFraFil(inn);
+    inn >> vegan;
+}
+
+/**
+ * skriver alle fløteis data til skjerm.
+ */
+void Floteis::skrivTilSkjerm() const{
+    Iskrem::skrivTilSkjerm();
+    cout << "vegan: " << (vegan ? "Ja" : "Nei") << "\n";
+}
+
+/**
+ * skrive alle fløteis data til fil.
+ */
+void Floteis::skrivTilFil(ofstream& ut) {
+    Iskrem::skrivTilFil(ut);
+    ut << vegan << "\n";
+}
+
+// ============================================================
+//  isbil – implementasjon
+// ============================================================
+
+Isbil::Isbil(ifstream& inn) {
+    inn >> sted;
 }
